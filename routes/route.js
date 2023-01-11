7ac721ffc101cda685529526b42c1ad83f3f6f0e
7twik <99895046+7twik@users.noreply.github.com>
2023-01-11 19:08:15 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app=express();
 const router = express.Router();
 const Form=require("../model/formModel");
-const {Formf ,BlogPost ,BlogGet, BlogGetP,FormGet } = require("../middleware/middleware");
+const {Formf ,BlogPost ,BlogGet, BlogGetP,FormGet, formDel,dateadd, dateSend} = require("../middleware/middleware");
 
 
 router.post("/form", Formf);
@@ -11,6 +11,9 @@ router.post("/AddBlog",BlogPost);
 router.get("/blog",BlogGet);
 router.get("/blogP",BlogGetP);
 router.get("/formGet",FormGet);
+router.post("/formdel",formDel);
+router.post("/date",dateadd);
+router.get("/dateget",dateSend)
 // router.route("/form").post((req,res)=>{ //note making
 // 	console.log(req.body);
 // });
